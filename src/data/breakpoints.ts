d5f8b0e1f3319a53543c2e60ec8ea496bdf50c77
donaldzhu <donaldzhu.toronto@gmail.com>
2023-10-17 03:27:30 -0400

    toggled working breakpt features

--- src/data/breakpoints.ts
+++ src/data/breakpoints.ts
@@ -1,19 +1,19 @@
 import { Breakpt } from '../utils/queryUtil'
 
-// const breakpts: Record<Breakpt, number> = {
-//   s: 0,
-//   m: 768,
-//   l: 1024,
-//   xl: 1280,
-//   xxl: 2560
-// }
-
 const breakpts: Record<Breakpt, number> = {
   s: 0,
   m: 768,
-  l: 992,
-  xl: 1200,
+  l: 1024,
+  xl: 1280,
   xxl: 2560
 }
+
+// const breakpts: Record<Breakpt, number> = {
+//   s: 0,
+//   m: 768,
+//   l: 992,
+//   xl: 1200,
+//   xxl: 2560
+// }
 
 export default breakpts
